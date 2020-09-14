af03314302c3b68c5f436094aee215535902f88f
liza hakem <liza.hakem@epitech.eu>
2020-09-14 17:24:34 +0200

    <commit>

---
+++ push_that.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#fisrt bash
+git add --all
+git commit -am "<commit>" 
+git push origin master
---
+++ task05/push_that.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#first bash
+
+git add * 
+git commit - am  "<commit>"
+git  push
